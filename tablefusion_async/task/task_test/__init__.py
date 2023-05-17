750a8c0f54b81f868ab79d44fbae247e23028171
slzhu <12-slzhu@users.noreply.gitlab.acemap.cn>
2023-05-17 18:31:58 +0800

    update

--- tablefusion_async/task/task_test/__init__.py
+++ tablefusion_async/task/task_test/__init__.py
@@ -1,22 +1,31 @@
 from tablefusion_async.main import app
 from tablefusion_async.common import BaseTask, TaskFailure
+from celery import current_task
 
 
 @app.task(bind=True, base=BaseTask)
 def test_1(self, paper_id: str, content_id: int = 0):
+    queue_name = current_task().request.delivery_info['routing_key']
+    print("queue_name : ", queue_name)
     print("1 : ", paper_id)
 
 
 @app.task(bind=True, base=BaseTask)
 def test_2(self, paper_id: str, content_id: int = 0):
+    queue_name = current_task().request.delivery_info['routing_key']
+    print("queue_name : ", queue_name)
     print("2 : ", paper_id)
 
 
 @app.task(bind=True, base=BaseTask)
 def test_3(self, paper_id: str, content_id: int = 0):
+    queue_name = current_task().request.delivery_info['routing_key']
+    print("queue_name : ", queue_name)
     print("3 : ", paper_id)
 
 
 @app.task(bind=True, base=BaseTask)
 def test_4(self, paper_id: str, content_id: int = 0):
+    queue_name = current_task().request.delivery_info['routing_key']
+    print("queue_name : ", queue_name)
     print("4 : ", paper_id)
