3ed7c82413b21109ce81cc8ec61af83729953a1a
slzhu <12-slzhu@users.noreply.gitlab.acemap.cn>
2023-05-17 18:07:58 +0800

    update

--- tablefusion_async/config/__init__.py
+++ tablefusion_async/config/__init__.py
@@ -85,6 +85,7 @@ CELERY_TASK_REJECT_ON_WORKER_LOST = True
 CELERY_ACKS_LATE = True
 CELERY_TASK_TRACK_STARTED = True
 CELERY_QUEUES = (
+    Queue('celery', Exchange('celery'), routing_key='celery', queue_arguments={'x-max-priority': 10}),
     Queue('test', Exchange('test'), routing_key='test', queue_arguments={'x-max-priority': 10}),
 )
 CELERY_ROUTES = {
