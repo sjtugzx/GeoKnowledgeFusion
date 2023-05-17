2e1c4a2d0aeb03ecb321b0af5f751f076df7d720
slzhu <12-slzhu@users.noreply.gitlab.acemap.cn>
2023-05-17 18:48:50 +0800

    update

--- tablefusion_async/task/task_test/__init__.py
+++ tablefusion_async/task/task_test/__init__.py
@@ -5,27 +5,19 @@ from celery import current_task
 
 @app.task(bind=True, base=BaseTask)
 def test_1(self, *args):
-    queue_name = current_task().request.delivery_info['routing_key']
-    print("queue_name : ", queue_name)
     print("1 : ", args)
 
 
 @app.task(bind=True, base=BaseTask)
 def test_2(self, *args):
-    queue_name = current_task().request.delivery_info['routing_key']
-    print("queue_name : ", queue_name)
     print("2 : ", args)
 
 
 @app.task(bind=True, base=BaseTask)
 def test_3(self, *args):
-    queue_name = current_task().request.delivery_info['routing_key']
-    print("queue_name : ", queue_name)
     print("3 : ", args)
 
 
 @app.task(bind=True, base=BaseTask)
 def test_4(self, *args):
-    queue_name = current_task().request.delivery_info['routing_key']
-    print("queue_name : ", queue_name)
     print("4 : ", args)
