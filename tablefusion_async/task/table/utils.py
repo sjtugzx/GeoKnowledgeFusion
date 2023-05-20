35cf3aba38b8be4001a3762867ba5e6a23cf3bfa
slzhu <12-slzhu@users.noreply.gitlab.acemap.cn>
2023-05-20 20:00:45 +0800

    update check_table_exists

--- tablefusion_async/task/table/utils.py
+++ tablefusion_async/task/table/utils.py
@@ -1,5 +1,5 @@
 from tablefusion_async import crud
-import PyPDF2
+import fitz  # PyMuPDF
 
 
 def table_overlap(
@@ -12,16 +12,47 @@ def table_overlap(
 
 
 # 判断pdf是否存在表格 paper_list 表添加一列
-def check_table_exists(file):
-    reader = PyPDF2.PdfFileReader(file)
-    num_pages = reader.numPages
+def check_table_exists(pdf_bytes):
+    doc = fitz.open(stream=pdf_bytes, filetype="pdf")
+    num_pages = doc.page_count
 
     for page_number in range(num_pages):
-        page = reader.getPage(page_number)
-        text = page.extractText()
-
+        page = doc.load_page(page_number)
+        text = page.get_text()
+        # print("page: ", page, text)
         # Replace this with your table detection logic
-        if 'table' in text.lower():
+        if 'Table' in text:
             return True
+        image_list = page.get_images()
+        for image_index, image in enumerate(image_list, start=1):
+            if is_table_image(image):
+                return True
 
     return False
+
+
+def is_table_image(image):
+    # Replace this with your table detection logic using image processing
+    # You can use techniques like template matching, contour detection, etc.
+    # Example: Check if the image has a certain size or aspect ratio
+    width = image["width"]
+    height = image["height"]
+    if width > 100 and height > 100:
+        return True
+
+    return False
+
+# def check_table_exists(pdf_path):
+#     with open(pdf_path, 'rb') as file:
+#         reader = PyPDF2.PdfFileReader(file)
+#         num_pages = reader.numPages
+#
+#         for page_number in range(num_pages):
+#             page = reader.getPage(page_number)
+#             text = page.extractText()
+#
+#             # Replace this with your table detection logic
+#             if 'table' in text.lower():
+#                 return True
+#
+#     return False
